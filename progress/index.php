1a334bed96f9a507e7283f414c60d1a5a271ba7c
Yash Kolambekar <yashskolambekar2015@gmail.com>
2023-12-09 18:02:28 +0530

    new branch created

---
+++ progress/index.php
@@ -0,0 +1,21 @@
+<?php
+include "../php/db.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>IDOL CS</title>
+  <link rel="stylesheet" href="../css/base.css?v=1.1" />
+</head>
+
+<body>
+  <div id="main_wrapper">
+    <p>In the new branch named progress</p>
+  </div>
+</body>
+
+</html>
